7dce5c280084bdb290ad67378100c7499f837120
Laura Baukol <laura.baukol@mac.com>
2017-03-05 14:35:34 -0700

    added pug

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 // const path = require ('path');
-// const keyPublishable = process.env.PUBLISHABLE_KEY;
-// const keySecret = process.env.SECRET_KEY;
-// const stripe = require("stripe")(keySecret);
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+const stripe = require("stripe")(keySecret);
+const pug = require('pug');
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -18,27 +19,27 @@ app.use(express.static('./'));
 const images = require('./routes/photos');
 app.use('/photos', images);
 
-// app.get("/", (req, res) =>
-//     res.render("index.pug", {
-//         keyPublishable
-//     }));
-//
-// app.post("/charge", (req, res) => {
-//     let amount = 500;
-//
-//     stripe.customers.create({
-//             email: req.body.stripeEmail,
-//             source: req.body.stripeToken
-//         })
-//         .then(customer =>
-//             stripe.charges.create({
-//                 amount,
-//                 description: "Sample Charge",
-//                 currency: "usd",
-//                 customer: customer.id
-//             }))
-//         .then(charge => res.render("charge.pug"));
-// });
+app.get("/", (req, res) =>
+    res.render("index.pug", {
+        keyPublishable
+    }));
+
+app.post("/charge", (req, res) => {
+    let amount = 1500;
+
+    stripe.customers.create({
+            email: req.body.stripeEmail,
+            source: req.body.stripeToken
+        })
+        .then(customer =>
+            stripe.charges.create({
+                amount,
+                description: "Sample Charge",
+                currency: "usd",
+                customer: customer.id
+            }))
+        .then(charge => res.render("charge.pug"));
+});
 
 // app.use('*', function(req, res, next) {
 //   console.log("Are we getting here?");
@@ -47,9 +48,9 @@ app.use('/photos', images);
 
 const port = process.env.PORT || 5280;
 
-app.get('/', (req, res, next) => {
-    console.log('Is this thing on?');
-});
+// app.get('/', (req, res, next) => {
+//     console.log('Is this thing on?');
+// });
 
 app.listen(port, () => {
     console.log('Listening on port', port);
